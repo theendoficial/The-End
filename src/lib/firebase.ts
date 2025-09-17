0cfb2475cee193803643eabaa226786fda352a9f
The End <theend.principal@gmail.com>
2025-09-17 19:08:03 +0000

    Falha no Login [DEBUG] Ocorreu um erro no servidor. Código: UNKNOWN_ERRO

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // This configuration now exclusively reads from public environment variables.
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
